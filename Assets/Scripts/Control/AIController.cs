3a66a8db002fd3a710a58f20c861246c6b47b34c
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-23 16:35:43 +0200

    Add AIController

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        GameObject player;
+
+        // Use this for initialization
+        void Start()
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) <= chaseDistance)
+            {
+                print($"{name} should chase {player.name}");
+            }
+        }
+    }
+}
