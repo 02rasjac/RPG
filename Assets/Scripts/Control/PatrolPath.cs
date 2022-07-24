ec635dc474be3b5de10259edcbddd4e2bac8cb01
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-24 17:27:18 +0200

    Change alpha on path-gizmo when not selected

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -9,12 +9,12 @@ namespace RPG.Control
 
         void OnDrawGizmos()
         {
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                Gizmos.color = i == 0 ? Color.green : Color.white;
-                Gizmos.DrawSphere(GetChildPosition(i), visualiseRadius);
-                Gizmos.DrawLine(GetChildPosition(i), GetChildPosition(GetNextIndex(i)));
-            }
+            RenderEachWaypoint(0.3f);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            RenderEachWaypoint(1.0f);
         }
 
         public Vector3 GetChildPosition(int index)
@@ -25,6 +25,18 @@ namespace RPG.Control
         public int GetNextIndex(int i)
         {
             return (i == transform.childCount - 1) ? 0 : (i + 1);
+        }
+
+        void RenderEachWaypoint(float alpha)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Color c = i == 0 ? Color.green : Color.white;
+                c.a = alpha;
+                Gizmos.color = c;
+                Gizmos.DrawSphere(GetChildPosition(i), visualiseRadius);
+                Gizmos.DrawLine(GetChildPosition(i), GetChildPosition(GetNextIndex(i)));
+            }
         }
     }
 }
