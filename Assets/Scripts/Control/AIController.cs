d86fdfc19352bf3a519519d9475de00091febfb8
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-24 11:40:28 +0200

    Add suspicion behavior

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -10,20 +10,24 @@ namespace RPG.Control
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 2f;
 
         Fighter fighter;
         Health health;
         Mover mover;
+        ActionScheduler actionScheduler;
 
         GameObject player;
 
         Vector3 guardLocation;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
 
         void Awake()
         {
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
             mover = GetComponent<Mover>();
+            actionScheduler = GetComponent<ActionScheduler>();
         }
 
         // Use this for initialization
@@ -41,13 +45,19 @@ namespace RPG.Control
                 float dist = Vector3.Distance(transform.position, player.transform.position);
                 if (dist <= chaseDistance && fighter.CanAttack(player))
                 {
+                    timeSinceLastSawPlayer = 0f;
                     fighter.Attack(player);
+                }
+                else if (timeSinceLastSawPlayer < suspicionTime)
+                {
+                    actionScheduler.CancelCurrentAction();
                 }
                 else
                 {
                     mover.StartMoveAction(guardLocation);
                 }
 
+                timeSinceLastSawPlayer += Time.deltaTime;
             }
         }
 
