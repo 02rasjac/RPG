37c7fe2f33058662caad7369212194cfee379b60
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-24 12:02:52 +0200

    Visualise patrolpath

---
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        const float visualiseRadius = 0.5f;
+
+        void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Transform child = transform.GetChild(i);
+                Gizmos.color = Color.white;
+                Gizmos.DrawSphere(child.position, visualiseRadius);
+            }
+        }
+    }
+}
