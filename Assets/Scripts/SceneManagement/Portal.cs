d0e56077b57955f5e2b0200cec687e7aa1df9b19
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-25 22:07:41 +0200

    Add spawnpoint on portals

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
-        [SerializeField] int sceneIndexToBuild = -1;
+        [SerializeField] int sceneIndex = -1;
+        [SerializeField] Transform spawnPoint;
 
         void OnTriggerEnter(Collider other)
         {
@@ -17,10 +19,31 @@ namespace RPG.SceneManagement
 
         IEnumerator Transition()
         {
+            int oldSceneIndex = SceneManager.GetActiveScene().buildIndex;
             DontDestroyOnLoad(this);
-            yield return SceneManager.LoadSceneAsync(sceneIndexToBuild);
-            print("Scene loaded");
+            yield return SceneManager.LoadSceneAsync(sceneIndex);
+
+            Portal otherPortal = GetOtherPortal(oldSceneIndex);
+            UpdatePlayer(otherPortal);
+
             Destroy(gameObject);
+        }
+
+        Portal GetOtherPortal(int oldIndex)
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>()) {
+                if (portal.sceneIndex == oldIndex) return portal;
+            }
+            return null;
+        }
+
+        void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            // Make sure character faces correct direcition. (Thanks Corey)
+            Vector3 directionVector = (otherPortal.spawnPoint.position - otherPortal.transform.position).normalized;
+            player.transform.rotation = Quaternion.LookRotation(directionVector);
         }
     }
 }
