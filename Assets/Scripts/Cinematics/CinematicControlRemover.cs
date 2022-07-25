98e668e6694ccec07b36199e3898032a6b681843
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-25 13:58:50 +0200

    Add eventhandlers for cinematics

---
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicControlRemover : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            GetComponent<PlayableDirector>().played += DisableControls;
+            GetComponent<PlayableDirector>().stopped += EnableControls;
+        }
+
+        void DisableControls(PlayableDirector pd)
+        {
+            print("Disable controls");
+        }
+        
+        void EnableControls(PlayableDirector pd)
+        {
+            print("Enable controls");
+        }
+    }
+}
