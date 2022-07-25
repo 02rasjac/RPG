d41984c276046b4dc44fb049a31ce1f5d1cfda9a
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-25 20:05:36 +0200

    Add 'portal' to change scene

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneIndexToBuild = -1;
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+
+            SceneManager.LoadScene(sceneIndexToBuild);
+        }
+    }
+}
