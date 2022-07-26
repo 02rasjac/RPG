7178a8b399420c487ff78989768d3a2602faf156
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-26 17:05:48 +0200

    Get path to data-folder

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace RPG.Saving
 {
     public class SavingSystem : MonoBehaviour
     {
+        const string fileExtension = ".sav";
+
         public void Save(string fileName)
         {
             print("Save to " + fileName);
@@ -14,6 +17,11 @@ namespace RPG.Saving
         public void Load(string fileName)
         {
             print("Load from " + fileName);
+        }
+
+        public string GetPathFromSaveFile(string saveFile)
+        {
+            return Path.Combine(Application.persistentDataPath, saveFile + fileExtension);
         }
     }
 }
--- Assets/Scripts/Saving/SavingWrapper.cs
+++ Assets/Scripts/Saving/SavingWrapper.cs
@@ -24,11 +24,11 @@ namespace RPG.Saving
         {
             if (save.WasPressedThisFrame())
             {
-                savingSystem.Save(defaultFileName);
+                savingSystem.Save(savingSystem.GetPathFromSaveFile(defaultFileName));
             }
             else if (load.WasPressedThisFrame())
             {
-                savingSystem.Load(defaultFileName);
+                savingSystem.Load(savingSystem.GetPathFromSaveFile(defaultFileName));
             }
         }
 
