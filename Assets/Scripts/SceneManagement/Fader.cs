e1c16ce6fce3ab3a0a85694c3fe17f95e5224167
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-26 13:33:11 +0200

    Add fader in code

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        [SerializeField] float fadeOutTime = 3f;
+        [SerializeField] float fadeInTime = 1f;
+
+        CanvasGroup canvasGroup;
+        void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+            StartCoroutine(FadeOutIn());
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return StartCoroutine(FadeOut());
+            yield return StartCoroutine(FadeIn());
+        }
+
+        IEnumerator FadeOut()
+        {
+            while (canvasGroup.alpha < 1f)
+            {
+                canvasGroup.alpha += Time.deltaTime / fadeOutTime;
+                yield return null;
+            }
+        }
+
+        IEnumerator FadeIn()
+        {
+            while (canvasGroup.alpha > 0f)
+            {
+                canvasGroup.alpha -= Time.deltaTime / fadeInTime;
+                yield return null;
+            }
+        }
+    }
+}
