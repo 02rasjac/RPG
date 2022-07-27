6377df8e6ca1c6dbe396185b5fab73bbe3901668
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-27 13:16:53 +0200

    Use C# BinaryFormatter for serialization

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using UnityEngine;
 
@@ -16,8 +17,9 @@ namespace RPG.Saving
             print("Save to " + path);
             using (FileStream fs = File.Open(path, FileMode.Create))
             {
-                byte[] buffer = SerializeVector(GetPlayerTransform().position);
-                fs.Write(buffer, 0, buffer.Length);
+                BinaryFormatter formatter = new BinaryFormatter();
+                SerializableVector3 sv3 = new SerializableVector3(GetPlayerTransform().position);
+                formatter.Serialize(fs, sv3);
             }
         }
     
@@ -27,37 +29,15 @@ namespace RPG.Saving
             print("Load from " + path);
             using (FileStream fs = File.Open(path, FileMode.Open))
             {
-                byte[] buffer = new byte[fs.Length];
-                fs.Read(buffer, 0, buffer.Length);
-                GetPlayerTransform().position = DeserializeVector(buffer);
+                BinaryFormatter formatter = new BinaryFormatter();
+                SerializableVector3 sv3 = (SerializableVector3)formatter.Deserialize(fs);
+                GetPlayerTransform().position = sv3.ToVector3();
             }
         }
 
         Transform GetPlayerTransform()
         {
             return GameObject.FindWithTag("Player").transform;
-        }
-
-        byte[] SerializeVector(Vector3 v)
-        {
-            ushort floatSize = sizeof(float);
-            byte[] buffer = new byte[3 * floatSize];
-            System.BitConverter.GetBytes(v.x).CopyTo(buffer, 0);
-            System.BitConverter.GetBytes(v.y).CopyTo(buffer, floatSize);
-            System.BitConverter.GetBytes(v.z).CopyTo(buffer, 2*floatSize);
-
-            return buffer;
-        }
-
-        Vector3 DeserializeVector(byte[] buffer)
-        {
-            Vector3 v = new Vector3();
-            ushort floatSize = sizeof(float);
-            v.x = System.BitConverter.ToSingle(buffer, 0);
-            v.y = System.BitConverter.ToSingle(buffer, floatSize);
-            v.z = System.BitConverter.ToSingle(buffer, 2 * floatSize);
-
-            return v;
         }
 
         string GetPathFromSaveFile(string saveFile)
