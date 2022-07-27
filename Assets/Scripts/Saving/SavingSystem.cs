802b1558e0a48b41b2753160ae94108a49864d03
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2022-07-27 12:34:57 +0200

    Save/load player-position to binary

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -16,7 +16,7 @@ namespace RPG.Saving
             print("Save to " + path);
             using (FileStream fs = File.Open(path, FileMode.Create))
             {
-                byte[] buffer = Encoding.UTF8.GetBytes("¡Hola Mundo!");
+                byte[] buffer = SerializeVector(GetPlayerTransform().position);
                 fs.Write(buffer, 0, buffer.Length);
             }
         }
@@ -29,8 +29,35 @@ namespace RPG.Saving
             {
                 byte[] buffer = new byte[fs.Length];
                 fs.Read(buffer, 0, buffer.Length);
-                print(Encoding.UTF8.GetString(buffer));
+                GetPlayerTransform().position = DeserializeVector(buffer);
             }
+        }
+
+        Transform GetPlayerTransform()
+        {
+            return GameObject.FindWithTag("Player").transform;
+        }
+
+        byte[] SerializeVector(Vector3 v)
+        {
+            ushort floatSize = sizeof(float);
+            byte[] buffer = new byte[3 * floatSize];
+            System.BitConverter.GetBytes(v.x).CopyTo(buffer, 0);
+            System.BitConverter.GetBytes(v.y).CopyTo(buffer, floatSize);
+            System.BitConverter.GetBytes(v.z).CopyTo(buffer, 2*floatSize);
+
+            return buffer;
+        }
+
+        Vector3 DeserializeVector(byte[] buffer)
+        {
+            Vector3 v = new Vector3();
+            ushort floatSize = sizeof(float);
+            v.x = System.BitConverter.ToSingle(buffer, 0);
+            v.y = System.BitConverter.ToSingle(buffer, floatSize);
+            v.z = System.BitConverter.ToSingle(buffer, 2 * floatSize);
+
+            return v;
         }
 
         string GetPathFromSaveFile(string saveFile)
