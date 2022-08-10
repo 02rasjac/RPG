f3c87b339a09788e32cbd1e59429f95dcf26bc90
02rasjac <rasmusjacklin2002@gmail.com>
2022-08-10 14:24:37 +0200

    Add base stats

---
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    public class BaseStats : MonoBehaviour
+    {
+        [Range(1, 99)]
+        [SerializeField] int startLevel = 1;
+        [SerializeField] CharacterClasses characterClass;
+    }
+}
