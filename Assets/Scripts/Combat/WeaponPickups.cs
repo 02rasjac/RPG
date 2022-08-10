daddc027ad4b666430d6b81eac07675f0823e903
02rasjac <rasmusjacklin2002@gmail.com>
2022-08-10 13:55:47 +0200

    Add respawning pickups

--- Assets/Scripts/Combat/WeaponPickups.cs
+++ Assets/Scripts/Combat/WeaponPickups.cs
@@ -7,12 +7,29 @@ namespace RPG.Combat
     public class WeaponPickups : MonoBehaviour
     {
         [SerializeField] Weapon weapon;
+        [SerializeField] float respawnTime = 5f;
         
         void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player")) return;
             other.GetComponent<Fighter>().EquipWeapon(weapon);
-            Destroy(this.gameObject);
+            StartCoroutine(HideAndShow());
+        }
+
+        IEnumerator HideAndShow()
+        {
+            SetActiveAndEnable(false);
+            yield return new WaitForSeconds(respawnTime);
+            SetActiveAndEnable(true);
+
+            void SetActiveAndEnable(bool value)
+            {
+                GetComponent<Collider>().enabled = value;
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).gameObject.SetActive(value);
+                }
+            }
         }
     }
 }
