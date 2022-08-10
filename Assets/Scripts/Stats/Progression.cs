b028c4eec7e284ee0df7dae86ab3fb676dbd1847
02rasjac <rasmusjacklin2002@gmail.com>
2022-08-10 14:47:41 +0200

    Add scriptable object for progression data

---
+++ Assets/Scripts/Stats/Progression.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    [CreateAssetMenu(menuName = "Stats/New progression")]
+    public class Progression : ScriptableObject
+    {
+        [SerializeField] ProgressionCharacterClass[] progressionCharacterClass;
+
+        [System.Serializable]
+        class ProgressionCharacterClass
+        {
+            [SerializeField] CharacterClasses characterClass;
+            [SerializeField] float[] health;
+        }
+    }
+}
