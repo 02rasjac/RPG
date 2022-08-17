203314dd3fb693f98d44fd40dfd323847feec0f3
02rasjac <rasmusjacklin2002@gmail.com>
2022-08-17 12:24:37 +0200

    Move audio listener

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] UnityEvent onHit;
         public void OnHit()
         {
-            print("Hitted");
+            onHit?.Invoke();
         }
     }
 }
