0bb62f79e6b50c465f6379f6041c0fc10cf83d11
02rasjac <rasmusjacklin2002@gmail.com>
2022-08-19 15:35:49 +0200

    Import Inventory asset pack

---
+++ Assets/Asset Packs/GameDev.tv Assets/Scripts/UI/ShowHideUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GameDevTV.UI
+{
+    public class ShowHideUI : MonoBehaviour
+    {
+        [SerializeField] InputAction toggleKey;
+        [SerializeField] GameObject uiContainer = null;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            uiContainer.SetActive(false);
+        }
+
+        void OnEnable()
+        {
+            toggleKey.Enable();
+        }
+        
+        void OnDisable()
+        {
+            toggleKey.Disable();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (toggleKey.WasPerformedThisFrame())
+            {
+                uiContainer.SetActive(!uiContainer.activeSelf);
+            }
+        }
+    }
+}
