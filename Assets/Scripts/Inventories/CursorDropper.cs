66e075bbf082bef37e1c8c8db329706bc777a93c
02rasjac <rasmusjacklin2002@gmail.com>
2022-08-20 12:23:16 +0200

    Make player drop on cursor

---
+++ Assets/Scripts/Inventories/CursorDropper.cs
@@ -0,0 +1,18 @@
+﻿using GameDevTV.Inventories;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts.Inventories
+{
+    public class CursorDropper : ItemDropper
+    {
+        protected override Vector3 GetDropLocation()
+        {
+            RaycastHit rayHit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue()), out rayHit))
+                return rayHit.point;
+            return transform.position;
+        }
+    }
+}
